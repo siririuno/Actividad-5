6e566ba2c5259a1a4448b61285e02fdb8e3230a8
Aaron Adolfo Gomez Redondo <143515444+siririuno@users.noreply.github.com>
2023-11-19 10:22:49 -0500

    Add files via upload

---
+++ principal.java
@@ -0,0 +1,9 @@
+package Notas;
+
+public class principal {
+    public static void main(String[] args) {
+        interfaz miVentanaPrincipal;
+        miVentanaPrincipal= new interfaz();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
